a5f2ff6857bcde6565c6a370bb7c2027da8c0488
Adam <a19224@yahoo.com.tw>
2016-09-02 05:20:15 +0000

    ADD Minesweeper

---
+++ Minesweeper123.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <form action="s.php" method="post">
+            <input type="text" name="maxBomb" value='10'/>炸彈數<br>
+            <input type="text" name="length" value='10'/>長<br>
+            <input type="text" name="width" value='10'/>寬<br>
+            <input type="submit" value="Submit"/>
+        </form>
+        
+    </body>
+</html>
